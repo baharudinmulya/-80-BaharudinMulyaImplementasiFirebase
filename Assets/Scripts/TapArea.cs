bb986abc24ee74b5ce6f56fa0659baa683692f90
baharudinmulya <baharu12185173@bsi.ac.id>
2021-10-03 23:46:22 +0700

    Step1

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+ 
+// [RequireComponent(typeof(AudioSource))]
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public AudioClip impact;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+        audioSource.PlayOneShot(impact, 1.0f);
+    }
+
+}
